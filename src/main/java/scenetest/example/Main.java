f5f016ef63893e9c3583a6a484ea843af3e9f1d5
josh samson <samsonknight909@gmail.com>
2025-02-01 21:06:19 -0500

    Created enum TOKEN

---
+++ src/main/java/scenetest/example/Main.java
@@ -0,0 +1,13 @@
+package scenetest.example;
+import java.util.Scanner;
+
+ enum TOKEN{
+    SCANEOF, ID, CONSTINT, VAR, OUTPUT, INITIALIZE, EQUALS, IF, THEN, ENDIF, COMPUTE, PLUS;
+        }
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        }
+    }
