9e26729ebcfd9a282aae96418270cd26194945f8
josh samson <samsonknight909@gmail.com>
2025-02-01 21:20:54 -0500

    Created List with values

--- src/main/java/scenetest/example/Main.java
+++ src/main/java/scenetest/example/Main.java
@@ -1,13 +1,16 @@
 package scenetest.example;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
  enum TOKEN{
     SCANEOF, ID, CONSTINT, VAR, OUTPUT, INITIALIZE, EQUALS, IF, THEN, ENDIF, COMPUTE, PLUS;
         }
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        List<String> reservedWords = Arrays.asList("var", "output", "initialize", "if", "then", "endif", "compute");
         }
     }
