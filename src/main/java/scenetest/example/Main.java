7fe04a86a953c0d3eed73e1ec8c864b70aba8ce6
josh samson <samsonknight909@gmail.com>
2025-02-01 21:43:23 -0500

    Added String Builder

--- src/main/java/scenetest/example/Main.java
+++ src/main/java/scenetest/example/Main.java
@@ -3,17 +3,20 @@ import java.io.PushbackReader;
 import java.io.StringReader;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
- enum TOKEN{
-    SCANEOF, ID, CONSTINT, VAR, OUTPUT, INITIALIZE, EQUALS, IF, THEN, ENDIF, COMPUTE, PLUS;
-        }
+class Scanner {
 
+
+    enum TOKEN {
+        SCANEOF, ID, CONSTINT, VAR, OUTPUT, INITIALIZE, EQUALS, IF, THEN, ENDIF, COMPUTE, PLUS;
+    }
+}
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         List<String> reservedWords = Arrays.asList("var", "output", "initialize", "if", "then", "endif", "compute");
         PushbackReader reader = new PushbackReader(new StringReader(""));
+        StringBuilder builder = new StringBuilder();
         }
     }
